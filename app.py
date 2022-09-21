6ad6a1316faecbaf89461c27bcc4ea9a06fc1759
VitorLima1 <vitorelgremio@gmail.com>
2022-09-21 10:37:12 -0300

    app.route estava com : e não /

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 from flask import Flask
 app = Flask("PROJETO")
-@app.route(";")
+@app.route("/")
 def ola_mundo():
     return "Olá Mundo! Esse é o primeiro projetinho Flask, 200"
 app.run()
