b6a6b85916104df1675a6231a0d002261d596e4a
VitorLima1 <vitorelgremio@gmail.com>
2022-09-21 11:10:52 -0300

    variavel nome criada

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ from flask import Flask, render_template
 app = Flask("PROJETO")
 @app.route("/")
 def ola_mundo():
-    return render_template("alo.html"), 200
+    #criar variavel nome
+    nome="Vitor Esbrolio Lima"
+    return render_template("alo.html", n=nome), 200
 app.run()
